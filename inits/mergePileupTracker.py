cd2fbe71f95e3e52e063e018c74cf46f1d608e99
jhrdinka <julia.hrdinka@cern.ch>
2018-06-29 17:51:15 +0200

    Add repository inits for tracker pile up merging and hits merging

---
+++ inits/mergePileupTracker.py
@@ -0,0 +1,2 @@
+path_to_INIT = '/afs/cern.ch/user/j/jhrdinka/work/public/FCCSW/init_v091.sh'
+path_to_FCCSW = '/afs/cern.ch/user/j/jhrdinka/work/public/FCCSW'
---
+++ inits/trackerMergedHits.py
@@ -0,0 +1,1 @@
+path_to_INIT = '/afs/cern.ch/user/j/jhrdinka/public/FCCSW/init_v091.sh'path_to_FCCSW = '/afs/cern.ch/user/j/jhrdinka/public/FCCSW'
